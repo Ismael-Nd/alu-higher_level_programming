bcd289d05dfe2b8b55d7f883731f2ee636cc612a
Ismael-Nd <n.ismael@alustudent.com>
2026-08-01 00:24:06 +0200

    Update curl command in 1-body.sh to follow redirects with -L option, ensuring proper response retrieval for URLs that redirect.

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Displays the response body of a URL, only when the status code is 200
-[ "$(curl -s -o /dev/null -w "%{http_code}" "$1")" = "200" ] && curl -s "$1"
+[ "$(curl -sL -o /dev/null -w "%{http_code}" "$1")" = "200" ] && curl -sL "$1"
