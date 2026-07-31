3f4c53fc5137343a9ea4735a889cb4167aed4e36
Ismael-Nd <n.ismael@alustudent.com>
2026-07-31 21:39:27 +0200

    Fix curl command in 0-body_size.sh to ensure silent mode is correctly applied by removing the redundant 'w' flag.

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Displays the size, in bytes, of the body of the response from a URL
-curl -sw "%{size_download}\n" -o /dev/null "$1"
+curl -s -w "%{size_download}\n" -o /dev/null "$1"
